fa39055087c2c633d52078ce12b3d09f7ff95978
pradyumn014 <pradyumna014@gmail.com>
2016-08-07 13:32:34 +0530

    quotations

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -17,7 +17,7 @@ VERIFY_TOKEN = '9871501397'
 
 quotes_arr = [["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse"],
 ["Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill"],
-["Work Hard","Albert Einstein"]
+["Work Hard","Albert Einstein"],
 ["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
 ["Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.", "Robert Frost"],
 ["I attribute my success to this: I never gave or took any excuse.", "Florence Nightingale"],
